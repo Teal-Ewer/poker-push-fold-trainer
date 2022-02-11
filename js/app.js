95e247e7916e4669499d72ae312d76bc4b877391
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-02-10 19:50:49 -0500

    add makeDeck function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
 /*-------------------------------- Constants --------------------------------*/
-const suits = ["spades", "hearts", "clubs", "diamonds"];
 const values = [2, 3, 4, 5, 6, 7, 8, 9, "T", "J", "Q", "K", "A"];
+const suits = ["spades", "hearts", "clubs", "diamonds"];
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let deck = [];
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*------------------------------- Functions --------------------------------*/
+class Card {
+	constructor(value, suit) {
+		this.value = value;
+		this.suit = suit;
+	}
+}
+
+function makeDeck() {
+	suits.forEach(suit => {
+		values.forEach(value => {
+			deck.push(new Card(value, suit));
+		});
+	});
+}
+
+makeDeck();
+console.log(deck);
