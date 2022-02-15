b6dcf850c19be3b0fbcad62cf504058fe008cbcf
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-02-15 08:48:29 -0500

    refactor chart3 and chart4

--- js/app.js
+++ js/app.js
@@ -4,73 +4,34 @@ const values = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 const suits = ["spades", "hearts", "clubs", "diamonds"];
 
 // prettier-ignore
-const mainChart = [
-  ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s"],
-  ["AKo", "KK", "KQs", "KJs", "KTs", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s"],
-  ["AQo", "KQo", "QQ", "QJs", "QTs", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "Q3s", "Q2s"],
-  ["AJo", "KJo", "QJo", "JJ", "JTs", "J9s", "J8s", "J7s", "J6s", "J5s", "J4s", "J3s", "J2s"],
-  ["ATo", "KTo", "QTo", "JTo", "TT", "T9s", "T8s", "T7s", "T6s", "T5s", "T4s", "T3s", "T2s"],
-  ["A9o", "K9o", "Q9o", "J9o", "T9o", "99", "98s", "97s", "96s", "95s", "94s", "93s", "92s"],
-  ["A8o", "K9o", "Q8o", "J8o", "T8o", "98o", "88", "87s", "86s", "85s", "84s", "83s", "82s"],
-  ["A7o", "K7o", "Q7o", "J7o", "T7o", "97o", "87o", "77", "76s", "75s", "74s", "73s", "72s"],
-  ["A6o", "K6o", "Q6o", "J6o", "T6o", "96o", "86o", "76o", "66", "65s", "64s", "63s", "62s"],
-  ["A5o", "K5o", "Q5o", "J5o", "T5o", "95o", "85o", "75o", "65o", "55", "54s", "53s", "52s"],
-  ["A4o", "K4o", "Q4o", "J4o", "T4o", "94o", "84o", "74o", "64o", "54o", "44", "43s", "42s"],
-  ["A3o", "K3o", "Q3o", "J3o", "T3o", "93o", "83o", "73o", "63o", "53o", "43o", "33", "32s"],
-  ["A2o", "K2o", "Q2o", "J2o", "T2o", "92o", "82o", "72o", "62o", "52o", "42o", "32o", "22"],
-];
-
-// prettier-ignore
+// (28 items long)
 const chart1 = [
-	"AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s",
-	"AKo", "KK", "KQs", "KJs", "KTs",
-	"AQo", "KQo", "QQ", "QJs", "QTs",
-	"AJo", "JJ", "JTs",
-	"ATo", "TT", "99", "88",
-	"77", "66","55", "44"
+	"AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "AKo", "KK", "KQs",
+	"KJs", "KTs", "AQo", "KQo", "QQ", "QJs", "QTs", "AJo", "JJ", "JTs",
+	"ATo", "TT", "99", "88", "77", "66", "55", "44"
 ]
 
 // prettier-ignore
+//(42 items long)
 const chart2 = chart1.concat([
-	"A7s", "A6s", "A5s", "A4s", "A3s", "A2s",
-	"K9s",
-	"Q9s",
-	"KJo", "J9s",
-	"T9s",
-	"A9o", "33", "22"
+	"A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "K9s", "Q9s", "KJo", "J9s",
+	"T9s", "A9o", "33", "22"
 ]);
 
 // prettier-ignore
-const chart3 = [
-	"AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s",
-	"AKo", "KK", "KQs", "KJs", "KTs", "K9s", "K8s", "K7s",
-	"AQo", "KQo", "QQ", "QJs", "QTs", "Q9s",
-	"AJo", "KJo", "QJo", "JJ", "JTs", "J9s", "J8s",
-	"ATo", "KTo", "QTo", "JTo", "TT", "T9s", "T8s",
-	"A9o", "99", "98s",
-	"A8o", "88",
-	"A7o", "77",
-	"A6o", "66",
-	"A5o", "55",
-	"A4o", "44", "33", "22"
-];
+// (56 items long)
+const chart3 = chart2.concat([
+	"K8s", "K7s", "QJo", "J8s", "KTo", "QTo", "JTo", "T8s", "98s", "A8o", 
+	"A7o", "A6o", "A5o", "A4o" 
+]);
 
 // prettier-ignore
-const chart4 = [
-	"AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s",
-	"AKo", "KK", "KQs", "KJs", "KTs", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s",
-	"AQo", "KQo", "QQ", "QJs", "QTs", "Q9s", "Q8s", "Q7s", "Q6s",
-	"AJo", "KJo", "QJo", "JJ", "JTs", "J9s", "J8s", "J7s",
-	"ATo", "KTo", "QTo", "JTo", "TT", "T9s", "T8s", "T7s",
-	"A9o", "K9o", "Q9o", "T9o", "99", "98s", "97s",
-	"A8o", "K8o", "88", "87s", "86s",
-	"A7o", "K7o", "77", "76s",
-	"A6o", "K6o", "66", "65s",
-	"A5o", "55",
-	"A4o", "44",
-	"A3o", "33",
-	"A2o", "22"
-];
+// (79 items long)
+const chart4 = chart3.concat([
+	"K6s", "K5s", "K4s", "K3s", "K2s", "Q8s", "Q7s", "Q6s", "J7s", "T7s",
+	"K9o", "Q9o", "T9o", "97s", "K8o", "87s", "86s", "K7o", "76s", "K6o",
+	"65s", "A3o", "A2o"
+]);
 
 // prettier-ignore
 const chart5 = [
